a5c53c9b6f320451f69c8209f526193242794d84
atilla <atilla1993@hotmail.se>
2024-08-01 13:58:36 +0200

    created articles route

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import categories from "./routes/categories";
+import articles from "./routes/articles";
 
 const app = express();
 
 app.use("/api/categories", categories);
-
+app.use("/api/articles", articles);
 const PORT = 5888;
 
 app.listen(PORT, () => console.log("Listening on port " + PORT));
---
+++ routes/articles.ts
@@ -0,0 +1,90 @@
+import express from "express";
+import { Category } from "./categories";
+
+const router = express.Router();
+
+export const articles: Article[] = [
+  {
+    id: "1-abcdid",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    nbrPages: 180,
+    runTimeMinutes: 0,
+    type: "Book",
+    isBorrowable: true,
+    category: { id: "11_catid", name: "Ficton" },
+  },
+  {
+    id: "2-abcdid",
+    title: "I Have No Mouth & I Must Scream",
+    author: "Harlan Ellison",
+    nbrPages: 0,
+    runTimeMinutes: 125,
+    type: "DVD",
+    isBorrowable: true,
+    category: { id: "22_catid", name: "Action" },
+  },
+  {
+    id: "3-abcdid",
+    title: "Where the Wild Things Are",
+    author: "Maurice Sendak",
+    nbrPages: 0,
+    runTimeMinutes: 120,
+    type: "Audiobook",
+    isBorrowable: false,
+    borrower: "Kalle Anka",
+    borrowDate: "2024-07-01",
+    category: { id: "33_catid", name: "Drama" },
+  },
+  {
+    id: "4-abcdid",
+    title: "I Am America",
+    author: "Stephen Colbert ",
+    nbrPages: 0,
+    runTimeMinutes: 300,
+    type: "DVD",
+    isBorrowable: false,
+    borrower: "Kalle Anka",
+    borrowDate: "2024-07-01",
+    category: { id: "44_catid", name: "Romantik" },
+  },
+  {
+    id: "5-abcdid",
+    title: "Blue Sisters",
+    author: "Coco Mellors ",
+    nbrPages: 280,
+    runTimeMinutes: 0,
+    type: "Book",
+    isBorrowable: true,
+    category: { id: "44_catid", name: "Romantik" },
+  },
+  {
+    id: "6-abcdid",
+    title: "Intermezzo",
+    author: "Sally Rooney",
+    nbrPages: 280,
+    runTimeMinutes: 0,
+    type: "Reference book",
+    isBorrowable: false,
+    category: { id: "44_catid", name: "Romantik" },
+  },
+];
+
+export interface Article {
+  id: string;
+  title: string;
+  runTimeMinutes: number;
+  author: string;
+  nbrPages: number;
+  type: string;
+  isBorrowable?: boolean;
+  category: Category;
+  borrower?: string;
+  borrowDate?: string;
+}
+
+router.get("/", (req, res) => {
+  return res.send(articles);
+});
+
+export default router;
